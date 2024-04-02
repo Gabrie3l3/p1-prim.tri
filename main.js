3d3a84b6e085c927ee1f83b5bda87319413a00ad
Gabrie3l3 <134063073+Gabrie3l3@users.noreply.github.com>
2024-04-02 15:21:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
-
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
+       
+   for(let j=0;<botoes.length;j++)     
+    botoes[j]. classlist.remove("ativo");
+    }
         
-        for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-        }
         botoes[i].classList.add("ativo");
     }
 }
