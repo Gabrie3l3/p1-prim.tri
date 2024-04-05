8126eed43cd26fd87de77d1f7c71bcb2785d40c7
Gabrie3l3 <134063073+Gabrie3l3@users.noreply.github.com>
2024-04-05 13:46:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+
+//Código omitido
 console.log(botoes);
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
        
    for(let j=0;<botoes.length;j++)     
-    botoes[j]. classlist.remove("ativo");
-    }
-        
+  
+       textos[j]. classlist.remove("ativo");
+ texto[j].classlist.add("ativo");    
         botoes[i].classList.add("ativo");
     }
 }
