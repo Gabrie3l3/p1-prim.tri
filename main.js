50037907c00a84ef7dde7cf76505a78e848fc325
Gabrie3l3 <134063073+Gabrie3l3@users.noreply.github.com>
2024-04-07 14:12:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -40,17 +40,13 @@ const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
         
 }
 function atualizaCronometro(){
-    documet.geE1ementbyid("dias0").calcularTempo
-        (tempo[1])[0];
-    document.getElementById("horas0").textContent = calculaTempo 
-        (tempos[1])[1];
-    document.getElementById("min0").textContent = calculaTempo
-        (tempos[1])[2];
-    document.getElementById("seg0").textContent = calculaTempo
-        (tempos[1])[3];
-    
+    documet.geE1ementbyid("dias0").calcularTempo(tempo[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
     for (let i = 0; i < contadores.length; i++) {
-    //contadores[i].textContent = calculaTempo(tempos[i]);
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+        console.log("seg"+i)
     }
 }
    
